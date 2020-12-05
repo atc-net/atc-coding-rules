279a12757989166b96be220c3b6d8768f45e749b
David Kallesen <david@lmdk.dk>
2020-12-05 20:47:39 +0100

    Update update-atc-coding-rules.ps1
    Added missing directory.build.targets and directory.build.props

--- update-atc-coding-rules.ps1
+++ update-atc-coding-rules.ps1
@@ -18,22 +18,32 @@ if ($isFirstTime)
     Write-Host "Please update build/common.props with you information"
 }
 
+Write-Host "Working on root folder"
 Invoke-WebRequest "https://raw.githubusercontent.com/atc-net/atc-coding-rules/main/distribution/build/code-analysis.props" -outfile "build/code-analysis.props"
 Invoke-WebRequest "https://raw.githubusercontent.com/atc-net/atc-coding-rules/main/distribution/.editorconfig" -outfile ".editorconfig"
 
 if (Test-Path sample -PathType Any)
 {
+    Write-Host "Working on sample folder"
     Invoke-WebRequest "https://raw.githubusercontent.com/atc-net/atc-coding-rules/main/distribution/sample/.editorconfig" -outfile "sample/.editorconfig"
+    Invoke-WebRequest "https://raw.githubusercontent.com/atc-net/atc-coding-rules/main/distribution/sample/directory.build.props" -outfile "sample/directory.build.props"
+    Invoke-WebRequest "https://raw.githubusercontent.com/atc-net/atc-coding-rules/main/distribution/sample/directory.build.targets" -outfile "sample/directory.build.targets"
 }
 
 if (Test-Path src -PathType Any)
 {
+    Write-Host "Working on src folder"
     Invoke-WebRequest "https://raw.githubusercontent.com/atc-net/atc-coding-rules/main/distribution/src/.editorconfig" -outfile "src/.editorconfig"
+    Invoke-WebRequest "https://raw.githubusercontent.com/atc-net/atc-coding-rules/main/distribution/src/directory.build.props" -outfile "src/directory.build.props"
+    Invoke-WebRequest "https://raw.githubusercontent.com/atc-net/atc-coding-rules/main/distribution/src/directory.build.targets" -outfile "src/directory.build.targets"
 }
 
 if (Test-Path test -PathType Any)
 {
+    Write-Host "Working on test folder"
     Invoke-WebRequest "https://raw.githubusercontent.com/atc-net/atc-coding-rules/main/distribution/test/.editorconfig" -outfile "test/.editorconfig"
+    Invoke-WebRequest "https://raw.githubusercontent.com/atc-net/atc-coding-rules/main/distribution/test/directory.build.props" -outfile "test/directory.build.props"
+    Invoke-WebRequest "https://raw.githubusercontent.com/atc-net/atc-coding-rules/main/distribution/test/directory.build.targets" -outfile "test/directory.build.targets"
 }
 
 Write-Host "Done..."
